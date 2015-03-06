f1d7fc64d752b9009c2bc0117e8952cd2929e19f
IanGibbs <ian@aprsworld.com>
2015-03-06 11:52:14 -0600

    resizing images and url link added

--- res/camWebGrid.js
+++ res/camWebGrid.js
@@ -1,6 +1,6 @@
 
 /*
--> sourceThumbnailURL
+-> sourceURL
         required
         URL where we get our primary image from
 
@@ -47,7 +47,7 @@
 
 */
 
-var sourceThumbnailURL;
+var sourceURL;
 var sourceFullURL;
 var sourceMetaJSON;
 var sourceMetaRefreshSeconds;
@@ -59,50 +59,80 @@ var sourceOverlayTextTop;
 var urlParamObjs;
 var cameraSeconds=[];
 
-/* This function takes the sourceThumbnailURL array and creates a grid based on it */
-function createCamBlocks( sourceThumbnailURL, sourceRefreshSeconds ) {
+/* This function takes the sourceURL array and creates a grid based on it */
+function createCamBlocks( sourceURL, sourceRefreshSeconds ) {
 
 	timers=[];
 
 	/* iterate through urls */
-	for ( var i = 0 ; i < sourceThumbnailURL.length ; i++ ) {
+	for ( var i = 0 ; i < sourceURL.length ; i++ ) {
+
+
+		var imageLink = "";
+
+		/* if sourceLinkToFullURL is set, then we want to add a link to the image when pressed */
+		if ( typeof sourceLinkToFullURL === 'undefined' ) {
+			imageLink = "<img id=\"cameraImage"+i+"\" src=\""+decodeURIComponent(sourceURL[i])+"\" >";
+		} else {
+			if ( typeof sourceLinkToFullURL[i] === 'undefined' ) {
+
+				imageLink = "<img id=\"cameraImage"+i+"\" src=\""+decodeURIComponent(sourceURL[i])+"\" >";
+			} else {
+				imageLink = "<a href=\""+sourceLinkToFullURL[i]+"\" ><img id=\"cameraImage"+i+"\" src=\""+decodeURIComponent(sourceURL[i])+"\" ></a>";				
+			}
+		}		
+
+		var overlay = "";
+
+		if ( typeof sourceOverlayTextTop !== 'undefined' ) {
+			if ( typeof sourceOverlayTextTop[i] !== 'undefined' ) overlay = "<span class=\"imageOverlay\">"+sourceOverlayTextTop[i]+"</span>";
+		}
 
 		/* create camera block */
-		$("#wrapper").append("<div class=\"gridBox\"><img src="+decodeURIComponent(sourceThumbnailURL[i])+" ><span>timer goes here</span><div>");
-		
+		$("#wrapper").append("<div class=\"gridBox\">"+imageLink+"<span class=\"imageTimer\">Updated <span id=\"timer"+i+"\"></span> ago.</span>"+overlay+"<div>");
+
+
+
 		/* create entry for camera seconds */
 		cameraSeconds[i]=0;
 		
 
 	}
-	
+	$(".gridBox").css( "width", (100/sourceURL.length*.9)+"%" );
 	/* start timer */
 	timerTick();
 
 }
 
+
+
 /* update the image at the given index */
 function updateCameraImg( index ) {
 
-	
+	//console.log("update camera at index: "+index);
+
+	/* this is for urls using latest.jpg */
+	$("#cameraImage"+index).attr("src",$("#cameraImage"+index).attr("src")+"?"+ new Date().getTime());
 
 }
 
 function timerTick(){
 
-	//console.log("tick");
 
 	/* iterate through camera seconds and increment the second count */
 	for (var i = 0 ; i < cameraSeconds.length ; i++ ) {
+
 		cameraSeconds[i]++;
-		console.log(cameraSeconds[i]);
+		$("#timer"+i).html(secToTime(cameraSeconds[i]));
+
 		/* if the second count equals sourceRefreshSeconds, update the image and reset second count */
+		
 		if ( cameraSeconds[i] >= sourceRefreshSeconds[i] ) {
 			cameraSeconds[i] = 0;
 			updateCameraImg(i);
-			console.log("update image at index: " + i );
 		}
 	}
+
 	/* call function again in 1 second */
 	setTimeout( timerTick, 1000 );
 
@@ -118,30 +148,74 @@ $( document ).ready(function(){
 	console.log(urlParamObjs);
 
 	/* Check for all required parameters */
-	if ( urlParamObjs.hasOwnProperty("sourceThumbnailURL") && urlParamObjs.hasOwnProperty("sourceRefreshSeconds") ) {
+	if ( urlParamObjs.hasOwnProperty("sourceURL") && urlParamObjs.hasOwnProperty("sourceRefreshSeconds") ) {
 
-		sourceThumbnailURL = urlParamObjs.sourceThumbnailURL;
+		sourceURL = urlParamObjs.sourceURL;
 		sourceRefreshSeconds = urlParamObjs.sourceRefreshSeconds;
 
 	} else {
 
-		console.log("cannot continue, missing sourceThumbnailURL or sourceRefreshSeconds");
-		$("#wrapper").append("<h1>Error: Missing one or more required parameters: sourceThumbnailURL or sourceRefreshSeconds</h1><br>");
+		console.log("cannot continue, missing sourceURL or sourceRefreshSeconds");
+		$("#wrapper").append("<h1>Error: Missing one or more required parameters: sourceURL or sourceRefreshSeconds</h1><br>");
 		return;
 
 	}
 
 	/* make sure the two required parameters are the same length */
-	if ( sourceThumbnailURL.length != sourceRefreshSeconds.length ) {
+	if ( sourceURL.length != sourceRefreshSeconds.length ) {
 
-		console.log("cannot continue, sourceThumbnailURL array is not the same size as sourceRefreshSeconds array");
-		$("#wrapper").append("<h1>Error: sourceThumbnailURL array size is not equal to sourceRefreshSeconds array</h1><br>");
+		console.log("cannot continue, sourceURL array is not the same size as sourceRefreshSeconds array");
+		$("#wrapper").append("<h1>Error: sourceURL array size is not equal to sourceRefreshSeconds array</h1><br>");
 		return;
 
 	}
 
+	
 
-	createCamBlocks(sourceThumbnailURL,sourceRefreshSeconds);
+	if ( urlParamObjs.hasOwnProperty("sourceFullURL") ) {
+
+		sourceFullURL = urlParamObjs.sourceFullURL;
+
+	}
+
+	if ( urlParamObjs.hasOwnProperty("sourceMetaJSON") ) {
+
+		sourceMetaJSON = urlParamObjs.sourceMetaJSON;
+
+	}
+	
+	if ( urlParamObjs.hasOwnProperty("sourceMetaRefreshSeconds") ) {
+
+		sourceMetaRefreshSeconds = urlParamObjs.sourceMetaRefreshSeconds;
+
+	}
+
+	if ( urlParamObjs.hasOwnProperty("sourceLinkToFullURL") ) {
+
+		sourceLinkToFullURL = urlParamObjs.sourceLinkToFullURL;
+
+	}
+
+	if ( urlParamObjs.hasOwnProperty("sourceLinkToFullURLBehavior") ) {
+
+		sourceLinkToFullURLBehavior = urlParamObjs.sourceLinkToFullURLBehavior;
+
+	}
+
+	if ( urlParamObjs.hasOwnProperty("sourceStaleSeconds") ) {
+
+		sourceStaleSeconds = urlParamObjs.sourceStaleSeconds;
+
+	}
+
+	if ( urlParamObjs.hasOwnProperty("sourceOverlayTextTop") ) {
+
+		sourceOverlayTextTop = urlParamObjs.sourceOverlayTextTop;
+
+	}
+
+
+	createCamBlocks(sourceURL,sourceRefreshSeconds);
 	
 
 });
