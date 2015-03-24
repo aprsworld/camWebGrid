898387850908e6fa7265b908c60d0926412a48c4
IanGibbs <ian@aprsworld.com>
2015-03-24 09:04:19 -0500

    started on settings generator

---
+++ res/generate_settings.js
@@ -0,0 +1,30 @@
+
+
+$( document ).ready( function() {
+
+	console.log("ready");
+
+});
+
+function addRow( rowId ) {
+
+	console.log(rowId);
+	$("#"+rowId).append('<input type="text" name="'+rowId+'[]" value="" />');
+
+}
+
+function addAll() {
+
+	addRow( 'sourceURL' );
+	addRow( 'sourceRefreshSeconds' );
+	addRow( 'sourceLinkToFullURL' );
+	addRow( 'sourceMetaJSON' );
+	addRow( 'sourceMetaRefreshSeconds' );
+	addRow( 'sourceLinkToFullURLBehavior' );
+	addRow( 'sourceStaleSeconds' );
+	addRow( 'sourceOverlayTextTop' );
+	addRow( 'sourceEXIF' );
+	addRow( 'sourceEXIFLabel' );
+
+}
+
